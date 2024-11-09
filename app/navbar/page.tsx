45d9a51e12c9c39d1cf294719a290c41b1a1bcf5
Alex Omaset <alexomaset711@gmail.com>
2024-11-09 19:45:24 +0300

    logo update

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -5,7 +5,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/images/logo.jpg";
+import lumi from "../public/images/lumi.jpeg";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,18 +16,15 @@ export default function Navbar() {
     <nav className="w-full bg-white px-6 py-4 text-black">
       <div className="flex justify-left items-center space-x-8">
         {/* Logo */}
-        <div className="flex items-center space-x-2">
-          <Image src={logo} alt="Lumicula logo" width={40} height={40} />
-          <h1 className="text-3xl lg:text-2xl font-extrabold tracking-tight text-black">
-            Lumicula
-          </h1>
-        </div>
+        <Link href="/">
+          <div className="flex items-center space-x-2">
+            <Image src={lumi} alt="Lumicula logo" width={150} height={100} />
+          </div>
+        </Link>
 
         {/* Centered Links */}
         <div className="hidden lg:flex space-x-6">
-          <Link href="/">
-            <p className="hover:text-yellow-300">Home</p>
-          </Link>
+          <Link href="/"></Link>
           <Link href="/feedback">
             <p className="hover:text-yellow-300">About</p>
           </Link>
@@ -45,11 +42,6 @@ export default function Navbar() {
       {/* Mobile Links */}
       {isOpen && (
         <div className="flex flex-col mt-4 space-y-2 lg:hidden">
-          <Link href="/">
-            <p onClick={toggleMenu} className="hover:text-yellow-300">
-              Home
-            </p>
-          </Link>
           <Link href="/feedback">
             <p onClick={toggleMenu} className="hover:text-yellow-300">
               About
